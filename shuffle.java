ffb10966098766bda390d083c70dd55b5f500b38
DarkKen12 <103071412+DarkKen12@users.noreply.github.com>
2023-10-10 20:08:18 +0530

    Update shuffle.java

--- shuffle.java
+++ shuffle.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class shuffle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        Random rd=new Random();
         int n=sc.nextInt();
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
-        Random rd=new Random();
         for(int i=0;i<n;i++)
         {   
             int idx=rd.nextInt(n);
